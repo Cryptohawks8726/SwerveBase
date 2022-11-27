e61f7f3db3613504af4305ec7bcd94c3953f3241
Nick Valerio <51347399+n-valerio@users.noreply.github.com>
2022-11-27 00:31:28 -0500

    add swerve constants enum

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -12,4 +14,49 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    
+    public static final class Swerve {
+        
+        // Physical Constants
+        public static final double driveBaseWidth = 2.0;
+        public static final double driveBaseLength = 2.0;
+        
+        // Electrical Constants
+        // TODO: add current limits
+        
+        // Controller Gains
+        // TODO: add PID + FF constants
+        
+        // Module Constants
+        public enum Module{
+            /*       
+            From Translation2d docs: 
+            When the robot is placed on the origin, facing toward the X direction, 
+            moving forward increases the X, whereas moving to the left increases the Y.
+
+            Mod{modPos,driveMotorid,steerMotorid,cancoderid,displacment(x,y)}
+            */
+            NE(0,0,0,0,new Translation2d(driveBaseLength/2,driveBaseWidth/2)), 
+            SE(1,0,0,0,new Translation2d(-driveBaseLength/2,driveBaseWidth/2)),
+            SW(2,0,0,0,new Translation2d(-driveBaseLength/2,-driveBaseWidth/2)),
+            NW(3,0,0,0,new Translation2d(driveBaseLength/2,-driveBaseWidth/2)); 
+            
+            public final int modPos;
+            public final int driveMotorid;
+            public final int steerMotorid;
+            public final int cancoderid;
+            public final Translation2d displacment; // from robot origin
+            
+            private Module(int modPos, int driveMotorid, int steerMotorid, int cancoderid, Translation2d displacment){
+                this.modPos = modPos;
+                this.driveMotorid = driveMotorid;
+                this.steerMotorid = steerMotorid;
+                this.cancoderid = cancoderid;
+                this.displacment = displacment;
+            }
+
+        }
+      
+    }
+}
