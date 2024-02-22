61096ef0fca5192b3420d6b8ac8ee792715c526c
source123x <110315300+source123x@users.noreply.github.com>
2024-02-22 17:49:41 -0500

    Almost working arm code

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -8,32 +8,33 @@ import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 
 public class ArmSubsystem extends SubsystemBase implements BooleanSupplier {
 
     int deviceId = 50;
-    int deviceId2 = 51;
+    int deviceId2 = 51; // secondary motor
     int shooterOffset = 135; // TODO: get real offset angle (around 135)
-    double intakeAngle = toRads(6.98); // TODO: get real angles to intake/shoot
-    double shootAngle = toRads(49); // at subwoofer
-    double initVel = toRads(14); // TODO: get initial velocity of shooter
+    double intakeRad = toRads(6.98); // TODO: get real angles to intake/shoot
+    double shootRad = toRads(49); // at subwoofer
+    double shooterVel = 14; // TODO: get initial velocity of shooter
     double maxAmpAngle = 121.5;
     double speakerHeight = 5.9812; // in meters
     double absEncoderOffset = 124.7233920;
     // TODO: get constants
-    double kp = 0.2;
+    double kp = 0.5;
     double ki = 0;
     double kd = 0;
 
-    double ks = 0.25;
+    double ks = 0.135;
     double kg = 0.33; //0.33
     double kv = 3.62; //3.62
     double ka = 0; //0.02
@@ -43,11 +44,14 @@ public class ArmSubsystem extends SubsystemBase implements BooleanSupplier {
     SparkAbsoluteEncoder absoluteEncoder = motorController.getAbsoluteEncoder(SparkAbsoluteEncoder.Type.kDutyCycle);
     
     Constraints constraints = new Constraints(0.3, 0.085); // TODO: find good constraints (in degrees/s)
-    ProfiledPIDController pidController = new ProfiledPIDController(kp, ki, kd, constraints,0.2); // degrees
+    PIDController pidController = new PIDController(kp, ki, kd); // degrees
     ArmFeedforward armFF = new ArmFeedforward(ks, kg, kv, ka);
-    State intakeState = new State(intakeAngle, 0);
-    State shootState = new State(shootAngle, 0);
-    State calculateAngleState = new State(shootAngle, 0); // will be changed based on calculated angle
+    State intakeState = new State(intakeRad, 0);
+    State shootState = new State(shootRad, 0);
+    State calculateAngleState = new State(shootRad, 0); // will be changed based on calculated angle
+
+    State goal = new State();
+    TrapezoidProfile trapezoidProfile = new TrapezoidProfile(constraints);
 
     public ArmSubsystem() {
         absoluteEncoder.setPositionConversionFactor(360); // "192 rotations per 1 rotation"
@@ -60,37 +64,39 @@ public class ArmSubsystem extends SubsystemBase implements BooleanSupplier {
 
         motorController.setInverted(true);
         motorController2.setInverted(false);
-
-        pidController.setGoal(toRads(getArmAngle()));
+        
+        goal = new State(getArmRad(),0);
+        pidController.setSetpoint(getArmRad());
     }
 
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("Arm Rad",getArmAngle());
-        double pidOutput = pidController.calculate(toRads(getArmAngle()));
-        State setpoint = pidController.getSetpoint();
+        SmartDashboard.putNumber("Arm Rad",getArmRad());
+        pidController.setSetpoint(goal.position);
+        double pidOutput = pidController.calculate(getArmRad());
+        State setpoint = trapezoidProfile.calculate(0.02, new State(getArmRad(),toRads(absoluteEncoder.getVelocity())), goal);
 
-        //double ff = armFF.calculate(setpoint.position-toRads(14), setpoint.velocity);
-        double ff =  ks + kg*Math.cos(setpoint.position-toRads(14)) + kv*setpoint.velocity;
+        double ff = armFF.calculate(setpoint.position-toRads(0), setpoint.velocity);
+        //double ff =  ks + kg*Math.cos(setpoint.position-toRads(14)) + kv*setpoint.velocity;
         motorController.setVoltage(pidOutput+ff);
         motorController2.setVoltage(pidOutput+ff);
 
-        SmartDashboard.putNumber("armDeg", getArmAngle());
-        SmartDashboard.putNumber("armVel", absoluteEncoder.getVelocity());
+        SmartDashboard.putNumber("armDeg", getArmDeg());
+        SmartDashboard.putNumber("armVelDeg", absoluteEncoder.getVelocity());
         SmartDashboard.putNumber("Applied Voltage", pidOutput+ff);
         SmartDashboard.putNumber("Set Pos",setpoint.position-toRads(14));
-        SmartDashboard.putNumber("Goal", pidController.getGoal().position);
+        SmartDashboard.putNumber("Goal", goal.position);
         SmartDashboard.putNumber("Set Velocity",setpoint.velocity);
         SmartDashboard.putNumber("FF", ff);
         SmartDashboard.putNumber("PID",pidOutput);
-        SmartDashboard.putNumber("error",pidController.getPositionError()*180/Math.PI);
-        SmartDashboard.putNumber("Setpoint diff",Math.abs(getArmAngle()-pidController.getGoal().position) );
+        SmartDashboard.putNumber("error",pidController.getPositionError());
+        SmartDashboard.putNumber("Setpoint diff",Math.abs(getArmRad()-goal.position) );
         //SmartDashboard.putNumber("calculatedAngle", calculateAngle(pidOutput, ff))
     }
 
     @Override
     public boolean getAsBoolean() { // will check if the arm angle is at the setpoint periodically
-        if (Math.abs(getArmAngleRad()-pidController.getGoal().position) < toRads(5)) {
+        if (Math.abs(getArmRad()-goal.position) < toRads(5)) {
             return true;
         } else {
             return false;
@@ -104,11 +110,11 @@ public class ArmSubsystem extends SubsystemBase implements BooleanSupplier {
     /**
      * in degrees
      */
-    public double getArmAngle() {
+    public double getArmDeg() {
         return absoluteEncoder.getPosition();
     }
 
-    public double getArmAngleRad() {
+    public double getArmRad() {
         return toRads(absoluteEncoder.getPosition());
     }
 
@@ -116,7 +122,7 @@ public class ArmSubsystem extends SubsystemBase implements BooleanSupplier {
      * in degrees
      */
     public double getShooterAngle() {
-        return 180-(getArmAngle()+shooterOffset);
+        return 180-(getArmDeg()+shooterOffset);
     }
 
     /**
@@ -124,7 +130,7 @@ public class ArmSubsystem extends SubsystemBase implements BooleanSupplier {
      */
     public Command rotateToIntake() { // sets the set point, angle to intake @ ground
         return new 
-        InstantCommand(() -> pidController.setGoal(intakeAngle));
+        InstantCommand(() -> goal = new State(intakeRad,0));
         /*
         .andThen(new WaitUntilCommand(this))
         .andThen(new InstantCommand(()->{
@@ -138,7 +144,7 @@ public class ArmSubsystem extends SubsystemBase implements BooleanSupplier {
      * in degrees
      */
     public InstantCommand rotateToShoot() { // angle to shoot @ subwoofer
-        return new InstantCommand(() -> pidController.setGoal(shootAngle)); // in degrees
+        return new InstantCommand(() -> goal = new State(shootRad,0)); // in degrees
     }
 
     /**
@@ -150,9 +156,9 @@ public class ArmSubsystem extends SubsystemBase implements BooleanSupplier {
         double horDist = Math.sqrt(posX*posX+posY*posY);
 
         // the angle that the shooter should be
-        double shooterAngle = Math.atan(initVel + Math.sqrt(Math.pow(initVel, 4) - 9.81*(9.81*horDist*horDist + 2*speakerHeight*initVel*initVel)) / (9.81*horDist));
+        double shooterAngle = Math.atan(shooterVel + Math.sqrt(Math.pow(shooterVel, 4) - 9.81*(9.81*horDist*horDist + 2*speakerHeight*shooterVel*shooterVel)) / (9.81*horDist));
         
-        double armAngle = 180 - shooterAngle - shooterOffset;
+        double armAngle = 180 - Math.toDegrees(shooterAngle) - shooterOffset;
         
         // TODO: consider shooting at an angle a little more because the hood is pretty big
         return armAngle;
@@ -164,7 +170,7 @@ public class ArmSubsystem extends SubsystemBase implements BooleanSupplier {
     public InstantCommand rotateToCalculatedAngle(double posX, double posY) {
         calculateAngleState.position = calculateAngle(posX, posY);
 
-        return new InstantCommand(() -> pidController.setGoal(calculateAngleState)); // in degrees
+        return new InstantCommand(() -> goal = calculateAngleState); // in degrees
     }
     
 }
