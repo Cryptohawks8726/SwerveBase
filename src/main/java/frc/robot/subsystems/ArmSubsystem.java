14b4df59258ae441f84bef9d86ad43ef388a175c
n-valerio <51347399+n-valerio@users.noreply.github.com>
2024-02-22 22:24:10 -0500

    Cleanup arm prior to merge

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -1,7 +1,5 @@
 package frc.robot.subsystems;
 
-import java.util.function.BooleanSupplier;
-
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.SparkAbsoluteEncoder;
 import com.revrobotics.CANSparkBase.IdleMode;
@@ -9,7 +7,6 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
@@ -18,7 +15,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ArmSubsystem extends SubsystemBase implements BooleanSupplier {
+public class ArmSubsystem extends SubsystemBase{
 
     int deviceId = 50;
     int deviceId2 = 51; // secondary motor
@@ -38,29 +35,30 @@ public class ArmSubsystem extends SubsystemBase implements BooleanSupplier {
     double kg = 0.33; //0.33
     double kv = 3.62; //3.62
     double ka = 0; //0.02
-
-    CANSparkMax motorController = new CANSparkMax(deviceId, MotorType.kBrushless); // motor controller
-    CANSparkMax motorController2 = new CANSparkMax(deviceId2, MotorType.kBrushless); // secondary motor
-    SparkAbsoluteEncoder absoluteEncoder = motorController.getAbsoluteEncoder(SparkAbsoluteEncoder.Type.kDutyCycle);
+    private final double DEG_TO_RAD = 0.017453292519943295; 
     
-    Constraints constraints = new Constraints(0.3, 0.085); // TODO: find good constraints (in degrees/s)
-    PIDController pidController = new PIDController(kp, ki, kd); // degrees
-    ArmFeedforward armFF = new ArmFeedforward(ks, kg, kv, ka);
-    State intakeState = new State(intakeRad, 0);
-    State shootState = new State(shootRad, 0);
-    State calculateAngleState = new State(shootRad, 0); // will be changed based on calculated angle
-
-    State goal = new State();
-    TrapezoidProfile trapezoidProfile = new TrapezoidProfile(constraints);
+    private final CANSparkMax motorController = new CANSparkMax(deviceId, MotorType.kBrushless);
+    private final CANSparkMax motorController2 = new CANSparkMax(deviceId2, MotorType.kBrushless);
+    private final SparkAbsoluteEncoder absoluteEncoder = motorController.getAbsoluteEncoder(SparkAbsoluteEncoder.Type.kDutyCycle);
+    
+    private final Constraints constraints = new Constraints(0.3, 0.085); // TODO: find good constraints (in degrees/s)
+    private final PIDController pidController = new PIDController(kp, ki, kd); // degrees
+    private final ArmFeedforward armFF = new ArmFeedforward(ks, kg, kv, ka);
+    private final TrapezoidProfile trapezoidProfile = new TrapezoidProfile(constraints);
+    private final State intakeState = new State(intakeRad, 0);
+    private final State shootState = new State(shootRad, 0);
+    private State calculateAngleState = new State(shootRad, 0); // will be changed based on calculated angle
+    private State goal = new State(); // will be changed to reflect current goal
+    
 
     public ArmSubsystem() {
-        absoluteEncoder.setPositionConversionFactor(360); // "192 rotations per 1 rotation"
+        absoluteEncoder.setPositionConversionFactor(360);
         absoluteEncoder.setVelocityConversionFactor(360);
         absoluteEncoder.setZeroOffset(absEncoderOffset);
         absoluteEncoder.setInverted(true);
 
-        motorController.setIdleMode(IdleMode.kCoast);
-        motorController2.setIdleMode(IdleMode.kCoast);
+        motorController.setIdleMode(IdleMode.kBrake); 
+        motorController2.setIdleMode(IdleMode.kBrake);
 
         motorController.setInverted(true);
         motorController2.setInverted(false);
@@ -71,31 +69,34 @@ public class ArmSubsystem extends SubsystemBase implements BooleanSupplier {
 
     @Override
     public void periodic() {
-        SmartDashboard.putNumber("Arm Rad",getArmRad());
+        
         pidController.setSetpoint(goal.position);
         double pidOutput = pidController.calculate(getArmRad());
         State setpoint = trapezoidProfile.calculate(0.02, new State(getArmRad(),toRads(absoluteEncoder.getVelocity())), goal);
+        double ff = armFF.calculate(setpoint.position-toRads(14), setpoint.velocity);//14 degrees accounts for offset from parallel 
 
-        double ff = armFF.calculate(setpoint.position-toRads(0), setpoint.velocity);
-        //double ff =  ks + kg*Math.cos(setpoint.position-toRads(14)) + kv*setpoint.velocity;
         motorController.setVoltage(pidOutput+ff);
         motorController2.setVoltage(pidOutput+ff);
 
+        SmartDashboard.putNumber("Arm Rad",getArmRad());
         SmartDashboard.putNumber("armDeg", getArmDeg());
         SmartDashboard.putNumber("armVelDeg", absoluteEncoder.getVelocity());
         SmartDashboard.putNumber("Applied Voltage", pidOutput+ff);
-        SmartDashboard.putNumber("Set Pos",setpoint.position-toRads(14));
+        SmartDashboard.putNumber("Set Pos",setpoint.position);
         SmartDashboard.putNumber("Goal", goal.position);
         SmartDashboard.putNumber("Set Velocity",setpoint.velocity);
         SmartDashboard.putNumber("FF", ff);
         SmartDashboard.putNumber("PID",pidOutput);
         SmartDashboard.putNumber("error",pidController.getPositionError());
         SmartDashboard.putNumber("Setpoint diff",Math.abs(getArmRad()-goal.position) );
-        //SmartDashboard.putNumber("calculatedAngle", calculateAngle(pidOutput, ff))
     }
 
-    @Override
-    public boolean getAsBoolean() { // will check if the arm angle is at the setpoint periodically
+    /*
+     * Returns true if the arm is within 5 degrees of the passed angle
+     * @param Ideal arm state, in radians
+     * @param True if the current angle is within 5 degrees of the goal
+     */
+    public boolean atStatePos(State goal) { // will check if the arm angle is at the setpoint periodically
         if (Math.abs(getArmRad()-goal.position) < toRads(5)) {
             return true;
         } else {
@@ -104,7 +105,7 @@ public class ArmSubsystem extends SubsystemBase implements BooleanSupplier {
     }
 
     public double toRads(double degrees) {
-        return degrees * Math.PI / 180;
+        return degrees * DEG_TO_RAD;
     }
 
     /**
@@ -126,22 +127,22 @@ public class ArmSubsystem extends SubsystemBase implements BooleanSupplier {
     }
 
     /**
-     * in degrees
+     * in radians
      */
     public Command rotateToIntake() { // sets the set point, angle to intake @ ground
         return new 
         InstantCommand(() -> goal = new State(intakeRad,0));
         /*
-        .andThen(new WaitUntilCommand(this))
+        .andThen(new WaitUntilCommand(()->this.atStatePos(goal)))
         .andThen(new InstantCommand(()->{
             motorController.setVoltage(0.0);
             motorController2.setVoltage(0.0);
             SmartDashboard.putString("At zero", "true");
-        })); // in degrees */
+        })); */
     }
 
     /**
-     * in degrees
+     * in radians
      */
     public InstantCommand rotateToShoot() { // angle to shoot @ subwoofer
         return new InstantCommand(() -> goal = new State(shootRad,0)); // in degrees
@@ -168,7 +169,7 @@ public class ArmSubsystem extends SubsystemBase implements BooleanSupplier {
      * calculates the angle to shoot to the speaker at any field pos and rotates the arm
      */
     public InstantCommand rotateToCalculatedAngle(double posX, double posY) {
-        calculateAngleState.position = calculateAngle(posX, posY);
+        calculateAngleState.position = toRads(calculateAngle(posX, posY));
 
         return new InstantCommand(() -> goal = calculateAngleState); // in degrees
     }
