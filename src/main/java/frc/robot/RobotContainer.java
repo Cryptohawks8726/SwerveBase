8f35363ef9f78ad6a0f943777a1f274586c59d53
Neb465 <120420307+Neb465@users.noreply.github.com>
2024-03-11 16:19:33 -0400

    pathfinding p gains

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -75,8 +75,8 @@ public class RobotContainer {
       NamedCommands.registerCommand("ShootFirstNote", new WaitCommand(2)); //shooter.fireNote(false) without remy
       NamedCommands.registerCommand("IntakeNoteCmd0", new WaitCommand(2));
       NamedCommands.registerCommand("IntakeNoteCmd1", new WaitCommand(2));
-      //NamedCommands.registerCommand("IntakeNoteCmd2", shooter.startIntake());
-      //NamedCommands.registerCommand("IntakeNoteCmd3", shooter.startFlywheels(5000));
+      NamedCommands.registerCommand("IntakeNoteCmd2", new WaitCommand(2));
+      NamedCommands.registerCommand("IntakeNoteCmd3", new WaitCommand(2));
       NamedCommands.registerCommand("ShootSecondNote", new WaitCommand(2));
       NamedCommands.registerCommand("ShootThirdNote", new WaitCommand(2));
       
