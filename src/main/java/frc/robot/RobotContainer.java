e7e181478bb0c4805e96f3108540e608ebd3f21d
DrewCodesBadly <dhruvak@outlook.com>
2025-12-11 17:46:15 -0500

    backport new swerve code and remove broken parts

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,66 +4,32 @@
 
 package frc.robot;
 
-import com.ctre.phoenix6.unmanaged.Unmanaged;
-import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.auto.NamedCommands;
-
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Command.InterruptionBehavior;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.util.Constants;
 
-import frc.robot.commands.ActualXboxTeleopDrive;
-import frc.robot.subsystems.SwerveDrive;
-
-
-/**
- * This class is where the bulk of the robot should be declared. Since Com
- * mand-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
- */
+// ROBOTCONTAINER CURRENTLY UP IN THE AIR
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final SwerveDrive drivetrain;
-
-  // private final CommandXboxController driverController;
-  
-  private final CommandXboxController driverController;
-  private final CommandXboxController operatorController;
   private final SendableChooser<Command> autoChooser;
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
-    Unmanaged.setPhoenixDiagnosticsStartTime(-1);
-    drivetrain = new SwerveDrive();
-    
-    driverController = new CommandXboxController(0);
-    operatorController = new CommandXboxController(1);
-
-    autoChooser = AutoBuilder.buildAutoChooser("EXAMPLE AUTO FILE NAME");
-    SmartDashboard.putData("Auto Chooser", autoChooser);
-    
-    NamedCommands.registerCommand("EXAMPLE_COMMAND", new PrintCommand(""));
-
+    autoChooser = new SendableChooser<>();
     configureButtonBindings();
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+   * it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    drivetrain.setDefaultCommand(new ActualXboxTeleopDrive(drivetrain,driverController).withInterruptBehavior(InterruptionBehavior.kCancelSelf));
-    
-    driverController.a().onTrue(drivetrain.resetGyroAngle());
-    driverController.rightBumper().whileTrue(drivetrain.passiveBrake());
   }
 
   /**
@@ -72,8 +38,10 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    if(!Constants.demoMode){
+    if (!Constants.demoMode) {
       return autoChooser.getSelected();
-    } else{ return null;}
- }
+    } else {
+      return null;
+    }
+  }
 }
---
+++ src/main/java/frc/robot/util/Constants.java
@@ -0,0 +1,44 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.util;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+    public static final boolean demoMode = false;
+
+    public static final class SwerveConstants {
+        public static final double maxSpeed = (demoMode ? 2.0 : 4.8); // m/s
+        public static final double maxAngularSpeed = (demoMode ? 1.5 : 3); // rad/s
+
+        public static final double defaultTranslationCoefficient = 1;
+        public static final double defaultRotationCoefficient = 1;
+        public static final double slowTranslationCoefficient = 0.1;
+        public static final double slowRotationCoefficient = 0.1;
+
+        public static final double positionkP = 0.5;
+        public static final double positionkI = 0.0;
+        public static final double positionkD = 0.0;
+
+        public static final double rotationkP = 0.5;
+        public static final double rotationkI = 0.0;
+        public static final double rotationkD = 0.0;
+
+        public static final double translationalErrorRange = 0.02;
+        public static final double rotationalErrorRange = 0.1; // Radians
+    }
+
+}
