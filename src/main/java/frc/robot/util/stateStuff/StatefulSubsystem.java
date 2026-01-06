f86e236d6bb3d32a10f7a3616b0d287bc4b56e2e
DrewCodesBadly <dhruvak@outlook.com>
2026-01-06 18:06:04 -0500

    push subsystems as sendables

--- src/main/java/frc/robot/util/stateStuff/StatefulSubsystem.java
+++ src/main/java/frc/robot/util/stateStuff/StatefulSubsystem.java
@@ -1,5 +1,6 @@
 package frc.robot.util.stateStuff;
 
+import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -15,25 +16,28 @@ public class StatefulSubsystem extends SubsystemBase {
 
     public StatefulSubsystem(String newSubsystemName) {
         subsystemName = newSubsystemName;
+        SmartDashboard.putData("Subsystems/" + newSubsystemName, this);
 
         executionTrigger = new Trigger(() -> supplier != null);
 
         executionTrigger
-            .onTrue(defer(() -> runOnce(() -> { 
-                activeCommand = supplier;
-                supplier = null;
-            })
-            .andThen(defer(() -> {
-                activeCommand.addRequirements(this);
-                return activeCommand;
-            })))
-            .ignoringDisable(true));
+                .onTrue(defer(() -> runOnce(() -> {
+                    activeCommand = supplier;
+                    supplier = null;
+                })
+                        .andThen(defer(() -> {
+                            activeCommand.addRequirements(this);
+                            return activeCommand;
+                        })))
+                        .ignoringDisable(true));
     }
 
     public void runNextCommand(Command toRun, boolean runsWhenDisabled) {
-        if (DriverStation.isDisabled() && runsWhenDisabled == false) return; 
+        if (DriverStation.isDisabled() && runsWhenDisabled == false)
+            return;
 
-        if (getCurrentCommand() != null) getCurrentCommand().cancel();
+        if (getCurrentCommand() != null)
+            getCurrentCommand().cancel();
 
         supplier = toRun;
 
@@ -44,9 +48,18 @@ public class StatefulSubsystem extends SubsystemBase {
 
     /**
      * Retrieves the current command, if one is currently running.
-     * @return The name of the command, if a name was provided. "None" if no command is running. The name of the command's class if it is unnamed.
+     * 
+     * @return The name of the command, if a name was provided. "None" if no command
+     *         is running. The name of the command's class if it is unnamed.
      */
     public String getCurrentCommandName() {
         return activeCommand != null ? currentCommandName : "None";
+    }
+
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        // Can uncomment if needed, but seems useless.
+        // super.initSendable(builder);
+        builder.addStringProperty("currentCommandName", this::getCurrentCommandName, null);
     }
 }
